4d4c112bf2085a969eb02d604f32ed9d94055ff5
leeroywking <leeroywking@gmail.com>
2019-06-25 21:31:37 -0700

    I can ride my bike with no handlebars

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const talkToEm = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
 console.log(talkToEm.greetEm('John'));
-console.log(math.add(1,3,5,6,7,8,8,5,4,3,3,5));
-console.log(math.subtract(1,3,5,7,7,8,9,6,5));
+console.log(math.add(1, 3, 5, 6, 7, 8, 8, 5, 4, 3, 3, 5));
+console.log(math.subtract(1, 3, 5, 7, 7, 8, 9, 6, 5));
 console.log(math.multiply(2));
-console.log(math.divide(1500,3));
+console.log(math.divide(1500, 3));
